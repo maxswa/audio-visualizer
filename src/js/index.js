5802749e1869b9fbf65468714080f6339675cbf1
maxswa <mkswartwout@gmail.com>
2018-09-26 23:55:11 -0400

    divide frequency array and animate lines with them

--- src/js/index.js
+++ src/js/index.js
@@ -35,7 +35,7 @@ window.onload = () => {
 
 		resizeCanvas();
 
-		const NUM_SAMPLES = 128;
+		const NUM_SAMPLES = 4096;
 		let audioElement = document.querySelector('audio');
 		let audioCtx = new AudioContext();
 		let analyzerNode = audioCtx.createAnalyser();
@@ -52,31 +52,76 @@ window.onload = () => {
 			let data = new Uint8Array(analyzerNode.frequencyBinCount);
 			analyzerNode.getByteFrequencyData(data);
 
-			const frequencies = {
-				bass: range(data,0,5),
-				low: range(data,6,20),
-				mid: range(data,21,35),
-				high: range(data,36,data.length)
-			};
+			const FREQUENCIES_PER_LINE = 32;
+			const LINE_AMOUNT = NUM_SAMPLES/FREQUENCIES_PER_LINE;
 
 			const center = {
 				x: canvas.width/2,
 				y: canvas.height/2
 			};
 
+      let lineArray = new Array(LINE_AMOUNT);
+
+      for (let i = 0; i < LINE_AMOUNT; i++) {
+        lineArray[i] = new Array(FREQUENCIES_PER_LINE);
+        for (let j = 0; j < FREQUENCIES_PER_LINE; j++) {
+          lineArray[i][j] = data[i*FREQUENCIES_PER_LINE+j];
+        }
+      }
 			ctx.clearRect(0,0,canvas.width,canvas.height);
 
+			ctx.strokeStyle = 'white';
+			for (let i = 0; i < LINE_AMOUNT; i++) {
+			  let yOffset =  center.y/3 + i*center.y/42;
+        ctx.beginPath();
+        let counterUp = 1;
+        let counterDown = 5;
+
+        if (i%2 !== 0) {
+          lineArray[i] = lineArray[i].reverse();
+        }
+
+        for (let j = 0; j < FREQUENCIES_PER_LINE; j++) {
+          let frequency = lineArray[i][j];
+          if(j < FREQUENCIES_PER_LINE/4 || j >= 3*FREQUENCIES_PER_LINE/4) {
+            frequency /= 10;
+          }
+          else {
+            frequency /= 2;
+          }
+
+          let random = Math.random() * (5 + 5) - 5;
+
+          if (j >= FREQUENCIES_PER_LINE/4 && j <= FREQUENCIES_PER_LINE/4 + 4) {
+            frequency /= 10/(counterUp * 2);
+            counterUp++;
+          }
+
+          if (j > 3*FREQUENCIES_PER_LINE/4 - 5 && j < 3*FREQUENCIES_PER_LINE/4) {
+            frequency /= 10/(counterDown * 2);
+            counterDown--;
+          }
+
+          ctx.lineTo(center.x/2 + j*center.x/32, yOffset - frequency);
+        }
+        ctx.stroke();
+      }
+
 			//Big Bass Circle
-			let grad = ctx.createRadialGradient(center.x,center.y,smaller(canvas.width,canvas.height)/5,
-				center.x,center.y,(smaller(canvas.width,canvas.height)/4)*(1+average(frequencies.bass)/500));
-			grad.addColorStop(0,'#e1ba74');
-			grad.addColorStop(1,'#232020');
-			ctx.fillStyle = grad;
-
-			//Low Range Circle
+			// let grad = ctx.createRadialGradient(center.x,center.y,smaller(canvas.width,canvas.height)/5,
+			// 	center.x,center.y,(smaller(canvas.width,canvas.height)/4)*(1+average(frequencies.bass)/500));
+			// grad.addColorStop(0,'#e1ba74');
+			// grad.addColorStop(1,'#232020');
+			// ctx.fillStyle = grad;
 
 
-			ctx.fillRect(0,0,canvas.width,canvas.height);
+
+			// ctx.fillRect(0,0,canvas.width,canvas.height);
+			// ctx.fillStyle = '#232020';
+			// ctx.beginPath();
+      // ctx.arc(center.x,center.y,smaller(canvas.width,canvas.height)/5,0,Math.PI*2);
+      // ctx.closePath();
+      // ctx.fill();
 		}
 
 		window.onresize = resizeCanvas;
